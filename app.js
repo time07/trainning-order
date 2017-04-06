afbf18c942f5e66a444fddba20a0b979e86a443e
Leonardo <leo.remigio92@gmail.com>
2017-04-06 20:23:54 -0300

    algumas modificações

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var app = angular.module("app", ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
     .when('/', {
         templateUrl:"templates/home.html",
         controller:"mainController"
     })
+    .when('/produto', {
+        templateUrl:"templates/produto.html",
+        controller:"produtoController"
+    })
+    .when('/produto/cadastro', {
+        templateUrl:"templates/produto-form.html",
+        controller:"produtoController"
+    })
     .otherwise({
         redirectTo: "/"
     });
+
 });
 
---
+++ controllers/produtoController.js
@@ -0,0 +1,5 @@
+app.controller("produtoController", produtoController);
+
+function produtoController($scope) {
+    
+}
